34a8d23842cfb77b2eab69d8e244a1a4df5c49f7
Funnyjoke <kirschz@hotmail.com>
2019-06-07 00:10:07 +0700

    initial

---
+++ MFECAssignment/Controllers/PromotionController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MFEC_Assignment.Controllers
+{
+    [Route("api/[controller]")]
+    public class PromotionController : Controller
+    {
+
+        [HttpGet("[action]")]
+        public IEnumerable<CustomerLog> ReadData()
+        {
+            var DataLog = System.IO.File.ReadAllLines(Path.Combine("", "data.json"));
+            List<CustomerLog> customerLogs = new List<CustomerLog>();
+            foreach (var log in DataLog)
+            {
+                CustomerLog customer = new CustomerLog();
+                string[] data = log.Split('|');
+                int i = 0;
+                double ServiceFee = 0;
+                customer.DateFormatted = data[i++];
+                customer.StartTime = DateTime.ParseExact(customer.DateFormatted +" "+data[i++], "dd/MM/yyyy HH:mm:ss",CultureInfo.InvariantCulture);
+                customer.EndTime = DateTime.ParseExact(customer.DateFormatted + " " + data[i++], "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                var diff = customer.EndTime - customer.StartTime;
+                if (diff.Minutes == 0 && diff.Seconds > 0 && diff.Hours == 0)
+                {
+                   ServiceFee = 3d;
+                }
+                else ServiceFee = 3d + (diff.Hours * 60d) + diff.Minutes + ((1 / 60d) * diff.Seconds) - 1d;
+
+                customer.ServiceFee = ServiceFee;
+                customer.DiffDate = (diff.Hours * 60) + diff.Minutes;
+                customer.ModileNumber = data[i++];
+                customer.Promotion = data[i++]; ;
+                customerLogs.Add(customer);
+            }
+            var groupedCustomerList = customerLogs
+            .GroupBy(u => u.ModileNumber)
+            .Select(g => new CustomerLog
+            {
+                ModileNumber = g.Key,
+                DiffDate = g.Sum(s => s.DiffDate),
+                ServiceFee = g.Sum(s => s.ServiceFee),
+                Promotion = g.First().Promotion,
+                DateFormatted = g.First().Promotion
+            })
+            .ToList();
+            var n = groupedCustomerList.Count;
+            return Enumerable.Range(1, n).Select(index => groupedCustomerList[index-1]);
+
+        }
+        public class CustomerLog
+        {
+            public string DateFormatted { get; set; }
+            public DateTime StartTime { get; set; }
+            public DateTime EndTime { get; set; }
+            public int DiffDate { get; set; }
+            public double ServiceFee { get; set; }
+            public string ModileNumber { get; set; }
+            public string Promotion { get; set; }
+        }
+    }
+}
